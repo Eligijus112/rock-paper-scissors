44a7f40b16477a7227407aeb26cf536ec47c448e
Eligijus <elasorama@gmail.com>
2020-09-19 10:30:32 +0300

    Fixed the dev files

--- backend-python/run.py
+++ backend-python/run.py
@@ -1,4 +1,18 @@
 from app import gameInstance
 
+# ENV variables
+from dotenv import load_dotenv
+
+# Importing os 
+import os 
+
+# Loading the dotenv file
+APP_ROOT = os.path.join(os.path.dirname(__file__))
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
 if __name__ == '__main__':
-    gameInstance.run()
+    gameInstance.run(
+        host = os.environ.get('HOST'),
+        port = os.environ.get('PORT')
+    )
